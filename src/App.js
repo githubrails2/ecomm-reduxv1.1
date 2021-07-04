41a518d5dd4670243992561c86b3b3e686a6d437
Bill White <soniclinuxknight@hotmail.com>
2021-07-03 21:34:44 -0500

    Fixed merge conflicts again

--- src/App.js
+++ src/App.js
@@ -5,17 +5,10 @@ import { Switch, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import HomepageLayout from "./layouts/HomepageLayout";
 import { auth, handleUserProfile } from "./firebase/utils";
-<<<<<<< HEAD
 import { useDispatch } from "react-redux";
 import { setCurrentUser } from "./redux/slices/userSlice";
 import WithAuth from "./HOC/WithAuth";
 const App = () => {
-=======
-import { useDispatch, useSelector } from "react-redux";
-import { selectCurrentUser, setCurrentUser } from "./redux/slices/userSlice";
-const App = () => {
-	const currentUser = useSelector(selectCurrentUser);
->>>>>>> main
 	const dispatch = useDispatch();
 	useEffect(() => {
 		const unsubscribe = auth.onAuthStateChanged(async (userAuth) => {
@@ -53,7 +46,6 @@ const App = () => {
 				/>
 				<Route
 					path="/registration"
-<<<<<<< HEAD
 					render={() => (
 						<MainLayout>
 							<Registration />
@@ -67,29 +59,6 @@ const App = () => {
 							<Login />
 						</MainLayout>
 					)}
-=======
-					render={() =>
-						currentUser ? (
-							<Redirect to="/" />
-						) : (
-							<MainLayout>
-								<Registration />
-							</MainLayout>
-						)
-					}
-				/>
-				<Route
-					path="/login"
-					render={() =>
-						currentUser ? (
-							<Redirect to="/" />
-						) : (
-							<MainLayout>
-								<Login />
-							</MainLayout>
-						)
-					}
->>>>>>> main
 				/>
 				<Route
 					path="/recovery"
