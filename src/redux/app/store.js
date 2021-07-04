d7cfb3b3e356b988c8a50c22133a22887b09dc2c
Bill White <soniclinuxknight@hotmail.com>
2021-07-03 21:53:07 -0500

    added and removed clg(addedmiddleware)

--- src/redux/app/store.js
+++ src/redux/app/store.js
@@ -2,6 +2,7 @@ import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import userReducer from "../slices/userSlice";
 
 const addedmiddleware = [...getDefaultMiddleware()];
+
 export default configureStore({
 	reducer: {
 		user: userReducer,
